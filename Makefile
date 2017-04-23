cedbd1c6e01818f45f8c8ab692af12fc5b5585c3
PublicHadyniak <public.hadyniak@gmail.com>
2017-04-23 15:31:58 +0300

    update install script

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ version:
 	@echo "libprogbase " & strings $(LIB_PATH) | grep Version -m 1 | cut -d " " -f 2
 
 update: 
-	@sh scripts/install_latest.sh
+	@sh scripts/install_latest_release.sh
 
 build:
 	cmake . && make
--- scripts/install_latest_release.sh
+++ scripts/install_latest_release.sh
@@ -2,15 +2,27 @@ GREEN='\033[1;32m'
 NC='\033[0m'
 
 echo "Updating libprogbase..."
+# pull latest repo code
 git pull origin master
+# fetch release tags from remote
 git fetch --tags
+# get latest release tag
 latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+# checkout to latest release code
 git checkout $latestTag
+
 printf "${GREEN}Installing libprogbase $latestTag:${NC}\n"
 echo "Building and installing libprogbase..."
-rm ./build -rf 
-mkdir ./build
+# clean and make build directory
+rm ./build -rf && mkdir ./build
+# build nd install static library with cmake
 cd ./build && cmake .. && make && sudo make install && cd ../
+# cleanup build directory
 rm ./build -rf
+
+# check installed library version
 printf "${GREEN}Checking installed version:${NC}\n"
 make version
+
+# return from git `detached HEAD` state
+git checkout master
