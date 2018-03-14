6a9d776c1e6fdba54bf913aa0e73b5987d248e9c
phadyniak <public.hadyniak@gmail.com>
2018-03-14 22:27:28 +0200

    fix events samples

--- src/clock.c
+++ src/clock.c
@@ -5,7 +5,7 @@
 #define _XOPEN_SOURCE 700  // to enable struct timespec and clock_gettime
 #include <time.h>
 
-#include <clock.h>
+#include <progbase/clock.h>
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 	#define CLOCK_REALTIME 0 
